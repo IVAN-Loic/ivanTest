8f4cf5826c9223dfc444ffecdb5e5dddd0701f6d
HUMBERTOMC <humberto.mattos@gmail.com>
2021-10-25 20:55:49 -0300

    Update PersonRepository.java

--- src/main/java/com/humbertomattos/springboot/repository/PersonRepository.java
+++ src/main/java/com/humbertomattos/springboot/repository/PersonRepository.java
@@ -3,9 +3,11 @@ package com.humbertomattos.springboot.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.humbertomattos.springboot.model.Person;
 
+@Repository
 public interface PersonRepository extends JpaRepository<Person, Long> {
   List<Person> findByActived(boolean actived);
 
